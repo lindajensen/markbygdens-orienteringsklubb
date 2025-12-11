331a267db2a1efc7773426f34c7f33f8ba80d52e
Linda Jensen <linda.jensen81@me.com>
2025-12-11 09:39:23 +0100

    Implemented reusable PageHero

--- frontend/src/pages/Sponsors.tsx
+++ frontend/src/pages/Sponsors.tsx
@@ -1,3 +1,6 @@
+import PageHero from "../components/PageHero";
+import sponsorsHero from "../assets/images/heroes/sponsors-hero.webp";
+
 import icaKvantum from "../assets/images/sponsors/ica-kvantum-skene.webp";
 import bilkonsulten from "../assets/images/sponsors/bilkonsulten.png";
 import sparbanken from "../assets/images/sponsors/sparbanken-sjuharad.webp";
@@ -7,31 +10,35 @@ import "./Sponsors.css";
 
 function Sponsors() {
   return (
-    <section className="sponsors">
-      <h1 className="sponsors__title">Vi tackar alla våra sponsorer</h1>
+    <>
+      <PageHero imageUrl={sponsorsHero} title="Sponsorer" />
+      
+      <section className="sponsors">
+        <h1 className="sponsors__title">Vi tackar alla våra sponsorer</h1>
 
-      <div className="sponsors__grid">
-        <div className="sponsors__card">
-          <img src={icaKvantum} alt="ICA Kvantum Skene" />
-          <p className="sponsors__name">ICA Kvantum Skene</p>
-        </div>
+        <div className="sponsors__grid">
+          <div className="sponsors__card">
+            <img src={icaKvantum} alt="ICA Kvantum Skene" />
+            <p className="sponsors__name">ICA Kvantum Skene</p>
+          </div>
 
-        <div className="sponsors__card">
-          <img src={bilkonsulten} alt="Bilkonsulten" />
-          <p className="sponsors__name">Bilkonsulten</p>
-        </div>
+          <div className="sponsors__card">
+            <img src={bilkonsulten} alt="Bilkonsulten" />
+            <p className="sponsors__name">Bilkonsulten</p>
+          </div>
 
-        <div className="sponsors__card">
-          <img src={sparbanken} alt="Sparbanken Sjuhärad" />
-          <p className="sponsors__name">Sparbanken Sjuhärad</p>
-        </div>
+          <div className="sponsors__card">
+            <img src={sparbanken} alt="Sparbanken Sjuhärad" />
+            <p className="sponsors__name">Sparbanken Sjuhärad</p>
+          </div>
 
-        <div className="sponsors__card">
-          <img src={markbladet} alt="Markbladet" />
-          <p className="sponsors__name">Markbladet</p>
+          <div className="sponsors__card">
+            <img src={markbladet} alt="Markbladet" />
+            <p className="sponsors__name">Markbladet</p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
