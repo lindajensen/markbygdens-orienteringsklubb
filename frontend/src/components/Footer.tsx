9eaa1c4be8c4dd2b86e55b3e84fe10ad54ffb71f
Linda Jensen <linda.jensen81@me.com>
2025-12-30 11:23:35 +0100

    Fix Footer Styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+// import { FaFacebook, FaInstagram } from "react-icons/fa";
 import "./Footer.css";
 
 function Footer() {
@@ -12,37 +12,6 @@ function Footer() {
             {" "}
             &copy; {currentYear} Markbygdens OK
           </small>
-        </div>
-        <div className="footer__social">
-          <ul className="footer__social-list">
-            <li className="footer__social-item">
-              <a
-                className="footer__social-link"
-                href="https://www.facebook.com/markbygdensok"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Besök oss på Facebook"
-              >
-                <FaFacebook
-                  aria-hidden="true"
-                  className="footer__social-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/markbygdensok/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Följ oss på Instagram"
-              >
-                <FaInstagram
-                  aria-hidden="true"
-                  className="footer__social-icon"
-                />
-              </a>
-            </li>
-          </ul>
         </div>
       </div>
     </footer>
